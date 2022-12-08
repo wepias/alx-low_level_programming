774bbe400ef32581c593ab6833e2fc2d226df98c
wepias <swbamba@gmail.com>
2022-12-08 21:57:47 +0200

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+	/* string initialisation */
+    char Mystr[ ] = "/"Programming is like building a multilingual puzzle";
+    
+    puts(Mystr); /* writting the string to stdout */
+
+    return 0;
+} 	

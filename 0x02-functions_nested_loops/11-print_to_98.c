20f28ca1e4a576ac7fd740f777ef812c891ea55b
wepias <swbamba@gmail.com>
2022-12-14 01:17:40 +0200

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,17 +3,37 @@
 /**
  * print_to_98 - function that prints all natural numbers from n to 98
  * @n: Number input
- *Return: Always 0
+ *Return: 0
  */
 void print_to_98(int n)
 {
-	for (int i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		printf("%d", i);
-		if (i < 98)
+		while (n <= 98)
 		{
-      			printf(", ");
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
 	}
 	printf("\n");
 }

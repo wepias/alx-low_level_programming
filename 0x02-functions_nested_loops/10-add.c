3da7f0068f6f3fc4453f26af347cf4b1cedcfa17
wepias <swbamba@gmail.com>
2022-12-14 00:43:39 +0200

    a b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,8 @@
 
 /**
  * add - function that adds two integers and returns the result
- *
- * B - The first number to add
- * A - The second number to add
- *
+ * @B: The first number to add
+ * @A: The second number to add
  * Return: The sum of B and A
  */
 

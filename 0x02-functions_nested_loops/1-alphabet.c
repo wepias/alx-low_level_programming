243bedb3727b9f79f906e6ea11fff85a2dcaee68
wepias <swbamba@gmail.com>
2022-12-13 08:35:23 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - print alphabet in lowercase
+ *  by wepias
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n ='a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+}

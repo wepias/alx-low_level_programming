dd0b81b6d665a63f1458ae3ccb55f5e522d47450
wepias <swbamba@gmail.com>
2022-12-15 22:36:29 +0200

    main h fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,9 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void;
+void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
-void more_numbers(void)
+void more_numbers(void);
 
 #endif

245fe11622017e91b9324937860b0b13c7f37f3c
wepias <swbamba@gmail.com>
2022-12-19 22:17:30 +0200

    very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - function that returns the length of a string
+ * @s: string
+ * Return : length of string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
+}

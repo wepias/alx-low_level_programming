a03a978e71fa65016385c9297053c9c750985637
wepias <swbamba@gmail.com>
2022-12-21 23:59:59 +0200

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @s: string
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}

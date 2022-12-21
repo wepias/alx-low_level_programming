89484c84e45eb4e3cf592f183996aba60a9ac788
wepias <swbamba@gmail.com>
2022-12-21 13:12:41 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - function that appends strings
+ * @src: character to append
+ * @dest: character to append
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return(dest);
+}

a6a83bfadb74e8eaabb62687cf53fd5fc578f026
wepias <swbamba@gmail.com>
2023-01-06 00:25:23 +0200

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of 
+ * _pow_recursion - function that returns the value of
  * x raised to the power of y
  * @x: interger
  * @y: int by which x value is raised

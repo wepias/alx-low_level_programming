829f737c9b09a11ebfc04a678ec36a8da9109b4e
wepias <swbamba@gmail.com>
2023-01-06 01:53:21 +0200

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,14 +3,26 @@
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: interger
+ * @i: interger
  *
  * Return: -1 if n does not have a natural square root
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int i)
 {
-	int i;
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i > n / 2)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion(n, i + 1);
+}
 
-	if (n < 0)
+int sqrt_recursion(int n)
+{
+	if (n <0)
 	{
 		return (-1);
 	}
@@ -18,12 +30,5 @@ int _sqrt_recursion(int n)
 	{
 		return (n);
 	}
-	for (i = 1; i <+ n / 2; i++)
-	{
-		if (i * i == n)
-		{
-			return (i);
-		}
-	}
-	return (-1);
+	return (_sqrt_recursion(n, 1));
 }

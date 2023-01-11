f3560286720b313074501fb8ad89373a0b4c7eba
wepias <swbamba@gmail.com>
2023-01-11 18:06:45 +0200

    alloc_grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,14 +14,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len1;
-	int len2;	
+	int len2;
 	char *result;
 
-	if (s1 == NULL) 
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2 == NULL) 
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}

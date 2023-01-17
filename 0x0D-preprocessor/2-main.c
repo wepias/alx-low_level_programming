395a44317a067502381e0fd1c656741010134828
wepias <swbamba@gmail.com>
2023-01-17 14:55:33 +0200

    Function-like macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the name of the file it was compiled from
+ * main - print the name of the file it was compiled from
  * followed by a new line
  *
  * Return: 0

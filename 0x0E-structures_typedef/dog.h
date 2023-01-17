2efd53b18683a89ce2d1139b9946663e8592869e
wepias <swbamba@gmail.com>
2023-01-17 15:29:18 +0200

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct - structure of type dog
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to the owners name
+ *
+ * Return: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

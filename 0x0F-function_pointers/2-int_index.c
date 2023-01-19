285fe4d42223bf0f7b8247d33edd3e5f907e1ca6
wepias <swbamba@gmail.com>
2023-01-19 22:06:46 +0200

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	if (size <= 0)
+	if (cmp != 0 && size >= 1 && array != 0)
 	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
+		if (size <= 0)
 		{
-			return (i);
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);

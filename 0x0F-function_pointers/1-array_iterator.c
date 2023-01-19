ac62d7cbebe68b95e460818391eba2c0519148f0
wepias <swbamba@gmail.com>
2023-01-19 21:36:16 +0200

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (action != 0 && size >= 1 && array != 0)
 	{
@@ -18,4 +18,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		{
 			action(array[i]);
 		}
+	}
 }

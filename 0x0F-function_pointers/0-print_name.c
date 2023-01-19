6e16e45464a9276c9feca5c164c3d1b58d8dead5
wepias <swbamba@gmail.com>
2023-01-19 21:12:14 +0200

    task 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,11 @@
 /**
  * print_name - function that prints a name.
  * @name: name of the person
- * *f: function pointer
+ * @f: function pointer
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-		f(name);
+	f(name);
 }

375bea78b80885e8b1f8e77c29640247670f7356
wepias <swbamba@gmail.com>
2023-01-19 18:02:38 +0200

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+
+#endif

3c5832ba70f8b80616ac2fd80fe559c703b4e6e2
wepias <swbamba@gmail.com>
2023-02-02 21:08:11 +0200

    task 9 & 10 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,71 @@
 #include "lists.h"
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * listint_len -  returns the number of elements in a linked listint_t list
+ * @h: head pointer of singly linked list
+ *
+ * Return: Number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int size = 0;
+
+	while (h != 0)
+		h = h->next, size++;
+
+	return (size);
+}
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to a pointer to the head of the list
- * @idx: index of the list where the new node should be added
- * @n: data to store in the new node
+ * @head: pointer to head of singly linked list
+ * @idx: index of the list where the new node should be added, starts at 0
+ * @n: value that the node will have
  *
- * Return: address of the new node
- * or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *temp, *new;
+	unsigned int len = listint_len(*head), count = 0;
+	listint_t *new_node, *tmp1, *tmp2;
 
-	temp = *head;
-	for (i = 0; temp != NULL && i < idx - 1; i++)
+	if (head == 0 && idx > 0)
+		return (0);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == 0)
+		return (0);
+
+	if (head == 0 && idx == 0)
 	{
-		temp = temp->next;
-	}
-	if (temp == NULL || (idx != 0 && temp->next == NULL))
-	{
-		return (NULL);
-	}
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		return (NULL);
+		new_node->n = n, new_node->next = 0, *head = new_node;
+		return (new_node);
 	}
 
-	new->n = n;
+	if (idx > len)
+	{
+		free(new_node);
+		return (0);
+	}
+	tmp1 = *head;
+	new_node->n = n;
+	while (count < idx && idx != 0)
+	{
+		tmp2 = tmp1, tmp1 = tmp1->next;
+		count++;
+	}
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
+		*head = new_node;
+		new_node->next = tmp1;
 	}
 	else
 	{
-		new->next = temp->next;
-		temp->next = new;
+		tmp2->next = new_node;
+		new_node->next = tmp1;
 	}
-	return (new);
+	return (new_node);
 }
